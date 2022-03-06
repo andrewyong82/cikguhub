cce05a8defb8a2176dc535a9098c1c211eb5bd22
fastlawmy <58140077+fastlawmy@users.noreply.github.com>
2022-03-07 03:24:38 +0800

    disable ssl

--- CikguHub/Startup.cs
+++ CikguHub/Startup.cs
@@ -100,10 +100,10 @@ namespace CikguHub
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
